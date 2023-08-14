e2ddabf1d7b9067437c3bc124400f8f0edfce2b2
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-08-14 13:16:29 -0300

    methor getById and id validator

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,12 +1,32 @@
 const User = require("../models/User");
+const { validationResult } = require("express-validator");
 
 class UserController {
     async getAll(req, res) {
         try {
             const results = await User.findAll();
-            res.json(results);
+            return res.json(results);
         } catch (err) {
-            res.status(500).json({ msg: "Não foi possível buscar os usuários.", error: err });                                     
+            return res.status(500).json({ msg: "Não foi possível buscar os usuários.", error: err });                                     
+        }
+    }
+
+    async getById(req, res) {
+        try {
+            const errors = validationResult(req);
+            if (!errors.isEmpty()) {
+                return res.status(400).json({ errors: errors.array() });
+            }
+
+            const user = await User.findById(req.params.id);
+            if (user) {
+                return res.status(200).json({ user });
+            }
+
+            return res.status(404).json({ msg: "Usuário inexistente." });
+
+        } catch (err) {
+            return res.status(500).json({ msg: "Não foi possível buscar o usuário.", error: err });
         }
     }
 }
--- routes.js
+++ routes.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const { param } = require("express-validator");
 const UserController = require("./controllers/UserController");
 const router = express.Router();
+
+const idParamValidator = [
+    param("id").isInt().withMessage("O ID deve ser um número inteiro.")
+];
 
 
 router.get("/", (req, res) => {
@@ -8,5 +13,6 @@ router.get("/", (req, res) => {
 });
 
 router.get("/user", UserController.getAll);
+router.get("/user/:id", idParamValidator, UserController.getById);
 
 module.exports = router;
